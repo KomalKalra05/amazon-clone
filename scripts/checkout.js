44e32a81246daaad3a97de89b712ce841314b372
Komal <tamannakalra20@gmail.com>
2024-11-03 17:51:06 +0530

    more changes

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart,removeFromCart} from '../data/cart.js';
+import {cart,removeFromCart,updateDeliveryOption} from '../data/cart.js';
 import {products} from '../data/products.js'
 import {deliveryOptions} from '../data/deliveryOptions.js';
 import formatCurrency from './utils/money.js';
@@ -90,7 +90,9 @@ function deliveryOptionsHTML(matchingProduct,cartItem){
         const priceString=deliveryOption.priceCents===0?'FREE':`$${formatCurrency(deliveryOption.priceCents)} -`;
         const isChecked=deliveryOption.id===cartItem.deliveryOptionId;
         html+=`                
-        <div class="delivery-option">
+        <div class="delivery-option js-delivery-option" 
+        data-product-id="${matchingProduct.id}"
+        data-delivery-option-id="${deliveryOption.id}">
             <input type="radio" ${isChecked? 'checked':''}
                 class="delivery-option-input"
                 name="delivery-option-${matchingProduct.id}">
@@ -106,3 +108,10 @@ function deliveryOptionsHTML(matchingProduct,cartItem){
     });
     return html;
 }
+
+document.querySelectorAll('.js-delivery-option').forEach((element)=>{
+    element.addEventListener('click',()=>{
+        const {productId,deliveryOptionId}=element.dataset;
+        updateDeliveryOption(productId,deliveryOptionId);
+    })
+})
