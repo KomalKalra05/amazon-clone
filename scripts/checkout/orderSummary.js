6ad56da9fca7be7d654efee76c6e5fb283b83bd4
Komal <tamannakalra20@gmail.com>
2024-11-13 11:01:47 +0530

    changes

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import {products,getProduct} from '../../data/products.js'
 import {deliveryOptions,getDeliveryOption} from '../../data/deliveryOptions.js';
 import formatCurrency from '../utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+import {renderPaymentSummary} from './paymentSummary.js'
 
 export function renderOrderSummary(){
 
@@ -68,7 +69,8 @@ export function renderOrderSummary(){
             const productId=link.dataset.productId;
             removeFromCart(productId);
             const container=document.querySelector(`.js-cart-item-container-${productId}`);
-            container.remove()
+            container.remove();
+            renderPaymentSummary();
         })
     })
 
@@ -105,6 +107,7 @@ export function renderOrderSummary(){
             const {productId,deliveryOptionId}=element.dataset;
             updateDeliveryOption(productId,deliveryOptionId);
             renderOrderSummary();
+            renderPaymentSummary();
         })
     })
 }
